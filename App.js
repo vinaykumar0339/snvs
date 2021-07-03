21873ee9daff54fc81d7117688976f57686b64fe
vinay kumar <vinay.kumar@mountblue.tech>
2021-07-03 18:09:04 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+  render() {
+    return <View><Text style={{color: 'white'}}>hello</Text></View>
+  }
+}
+
+export default App;
